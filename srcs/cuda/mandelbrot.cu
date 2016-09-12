f557c8a6f99fdd8daf3c6e241d2c5bf965495dfc
gundz <fgundlac@student.42.fr>
2016-09-12 14:04:30 +0200

    updated mandelbrot

--- srcs/cuda/mandelbrot.cu
+++ srcs/cuda/mandelbrot.cu
@@ -15,48 +15,29 @@ mandelbrot_kernel(t_cuda cuda, t_mandelbrot mandelbrot)
 		return ;
 
 	//each iteration, it calculates: newz = oldz*oldz + p, where p is the current pixel, and oldz stars at the origin
-	double pr, pi;           //real and imaginary part of the pixel p
-	double newRe, newIm, oldRe, oldIm;   //real and imaginary parts of new and old z
-
+	double cr, ci;           //real and imaginary part of the pixel p
+	double zr, zi;
+	double temp;
 	//calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
-	pr = 1.5 * (x - cuda.rx / 2) / (0.5 * mandelbrot.zoom * cuda.rx) + mandelbrot.moveX;
-	pi = (y - cuda.ry / 2) / (0.5 * mandelbrot.zoom * cuda.ry) + mandelbrot.moveY;
-	newRe = newIm = oldRe = oldIm = 0; //these should start at 0,0
+	cr = 1.5 * (x - cuda.rx / 2) / (0.5 * mandelbrot.zoom * cuda.rx) + mandelbrot.moveX;
+	ci = (y - cuda.ry / 2) / (0.5 * mandelbrot.zoom * cuda.ry) + mandelbrot.moveY;
+	zr = zi = 0;
 
-	int i;
-	//start the iteration process
-	for(i = 0; i < mandelbrot.maxIteration; i++)
+	int i = 0;
+	while ((zr * zr + zi * zi < 4) && i < mandelbrot.maxIteration)
 	{
-		//remember value of previous iterations
-		oldRe = newRe;
-		oldIm = newIm;
-		//the actual iteration, the real and imaginary part are calculated
-		newRe = oldRe * oldRe - oldIm * oldIm + pr;
-		newIm = 2 * oldRe * oldIm + pi;
-		//if the point is outside the circle with radius 2: stop
-		if ((newRe * newRe + newIm * newIm) > 4)
-			break;
+		temp = zr * zr - zi * zi + cr;
+		zi = (2 * zr * zi + ci);
+		zr = temp;
+		i++;
 	}
-	cuda.screen[dim_i] = mandelbrot.palette[(i + 1) % 255];
-}
-
-static void
-set_palette(int palette[256])
-{
-	palette[0] = 0;
-	for (int i = 1; i < 256; i++)
-	{
-		palette[i] = (int)(i + 512 - 512 * expf(-i / 50.0) / 3.0);
-		palette[i] = palette[i] << 24 | i % 255 << 16 | palette[i] % 255 << 8 | 255;
-	}
+	cuda.screen[dim_i] = (i % 256) << 24 | (i % 256) << 16 | (i % 256) << 8 | 255;
 }
 
 int
 mandelbrot_call(t_data *data, t_cuda *cuda)
 {
 	static t_mandelbrot	mandelbrot = {1, -0.5, 0, 300, {0}};
-
-	set_palette(mandelbrot.palette);
 
 	if (data->esdl->en.in.key[SDL_SCANCODE_LEFT] == 1)
 		mandelbrot.moveX -= 0.01 / mandelbrot.zoom * 10;
